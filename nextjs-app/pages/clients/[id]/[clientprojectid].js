2b5d2272d7585e7d48a75b961ecd7bef1f0f23bb
Danijel Avramov <danijel.avramov@gmail.com>
2021-03-07 17:00:33 +0100

    clean slate

--- nextjs-app/pages/clients/[id]/[clientprojectid].js
+++
@@ -1,9 +0,0 @@
-const SelectedClientProjectPage = () => {
-    return (
-        <div>
-            <h1>The Project Page for a specific project for selected client</h1>
-        </div>
-    );
-}
- 
-export default SelectedClientProjectPage;
--- nextjs-app/pages/index.js
+++ nextjs-app/pages/index.js
@@ -1,20 +1,8 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
-      <ul>
-        <li>
-          <Link href='/portfolio'>Portfolio</Link>
-        </li>
-        <li>
-          <Link href='/clients'>Clients</Link>
-        </li>
-        <li>
-          <Link href='/blog'>Blog</Link>
-        </li>
-      </ul>
+      
     </div>
   )
 }
